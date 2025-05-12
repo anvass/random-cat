b445d06e7fcbd6a5962a7af05bbea275b24d3599
anvass <nastya_0809@mail.ru>
2025-05-12 21:28:24 +0300

    feat: button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import Button from './components/Button/Button';
+
+const handleClick = () => {
+  console.log('Click');
+};
+
 function App() {
   return (
     <>
       <div>App</div>
+      <Button disabled onClick={handleClick}>
+        <span>Get cat</span>
+      </Button>
     </>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import { type JSX } from 'react';
+import styles from './Button.module.css';
+
+interface ButtonProps {
+  disabled?: boolean;
+  onClick?: () => void;
+  children: JSX.Element;
+}
+
+function Button({ disabled, onClick, children }: ButtonProps) {
+  return (
+    <button
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
+      className={`${styles.btn} ${disabled ? styles.disabled : ''}`}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
